fcc2bb5eb829a3b2241e7c259861962fbd68a813
nguyenhathanh2212 <thanhtdk2212@gmail.com>
2018-01-03 23:19:37 +0700

    init-admintemplate

---
+++ resources/views/templates/admin/master.blade.php
@@ -0,0 +1,5 @@
+@include ('templates.admin.header')
+    <div class="content">
+        @yield ('content')
+    </div>
+@include ('templates.admin.footer')
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,8 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .js('resources/assets/templates/elearning/js/script.js', 'public/templates/elearning/js/script.js')
-   .styles('resources/assets/templates/elearning/css/style.css', 'public/templates/elearning/css/style.css');
+   .styles('resources/assets/templates/elearning/css/style.css', 'public/templates/elearning/css/style.css')
+   .styles('resources/assets/templates/admin/css/style.css', 'public/templates/admin/css/style.css')
+   .styles('resources/assets/templates/admin/css/reset.css', 'public/templates/admin/css/reset.css');
 mix.copyDirectory('resources/assets/templates/elearning/images', 'public/templates/elearning/images');
+mix.copyDirectory('resources/assets/templates/admin/images', 'public/templates/admin/images');
